7da854fa323d8e253e737245bfd9858a03d50eee
unknown <andyh_007@hotmail.com>
2014-10-06 23:25:31 +0200

    engines node

--- config/db.js
+++ config/db.js
@@ -4,4 +4,5 @@
   var dbName = 'dbToollife'
  
   // the application is executed on the local machine ...
-  mongoose.connect('mongodb://localhost/' + dbName);
+  //mongoose.connect('mongodb://localhost/' + dbName);
+  mongoose.connect('mongodb://user:%BbddM0NG0L4B%@ds063909.mongolab.com:63909/heroku_app30445385');
